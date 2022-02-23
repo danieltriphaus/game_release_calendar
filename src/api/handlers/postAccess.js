afc61778a275011d98736a80ba9e2edee5bb0edb
danieltriphaus <d.triphaus@outlook.de>
2022-02-23 16:51:16 +0100

    get igdb access_token and set as cookie

--- src/api/handlers/postAccess.js
+++ src/api/handlers/postAccess.js
@@ -1,7 +1,40 @@
-export const postAccess = (context, req, res) => {
+import axios from "axios";
+
+const COOKIE_OPTIONS = {
+    httpOnly: true,
+    secure: true,
+};
+
+export const postAccess = async (context, req, res) => {
     if (req.body.apiKey === process.env.API_KEY) {
-        res.cookie("api_key", req.body.apiKey);
-        res.status(200).json();
+        const response = await axios
+            .post(
+                "https://id.twitch.tv/oauth2/token?client_id=" +
+                    +"&client_secret=" +
+                    process.env.IGDB_API_CLIENT_SECRET +
+                    "&grant_type=client_credentials"
+            )
+            .catch((error) => {
+                if (error.response) {
+                    res.status(error.response.data.status).json({
+                        error: "external_request",
+                        message: error.response.data.message,
+                    });
+                }
+            });
+
+        if (response) {
+            res.cookie("api_key", req.body.apiKey, {
+                maxAge: response.data.expires_in * 1000,
+                ...COOKIE_OPTIONS,
+            });
+
+            res.cookie("igdb_access_token", response.data.access_token, {
+                maxAge: response.data.expires_in * 1000,
+                ...COOKIE_OPTIONS,
+            });
+            res.status(200).json();
+        }
     } else {
         res.status(401).json({ error: "invalid_api_key", message: "Invalid API Key" });
     }
--- tests/unit/api/handlers/postAccess.spec.js
+++ tests/unit/api/handlers/postAccess.spec.js
@@ -1,26 +1,64 @@
 import { postAccess } from "@/api/handlers/postAccess";
 import { getContext, getRequest, getResponse } from "../expressMocks";
+import axios from "axios";
+
+jest.mock("axios");
 
 const context = getContext("post-access");
 
-it("should return 401 with error if apiKey is invalid", () => {
+it("should return 401 with error if apiKey is invalid", async () => {
     const response = getResponse();
     const request = getRequest();
     request.body.apiKey = "invalid_api_key";
 
-    postAccess(context, request, response);
+    await postAccess(context, request, response);
 
     expect(response.status).toHaveBeenCalledWith(401);
     expect(response.json).toHaveBeenCalledWith(expect.objectContaining({}));
 });
 
-it("should return 200 and set cookie if apiKey is valid", () => {
+it("should return 200 and set cookies with api_key and igdb access token if apiKey is valid", async () => {
     const response = getResponse();
     const request = getRequest();
     request.body.apiKey = "test_api_key";
 
-    postAccess(context, request, response);
+    const tokenResponse = {
+        access_token: "prau3ol6mg5glgek8m89ec2s9q5i3i",
+        expires_in: 5587808,
+        token_type: "bearer",
+    };
 
+    axios.post.mockResolvedValueOnce({ data: tokenResponse });
+
+    await postAccess(context, request, response);
+
+    expect(response.cookie).toHaveBeenCalledWith("api_key", request.body.apiKey, {
+        maxAge: tokenResponse.expires_in * 1000,
+        httpOnly: true,
+        secure: true,
+    });
+    expect(response.cookie).toHaveBeenCalledWith("igdb_access_token", tokenResponse.access_token, {
+        maxAge: tokenResponse.expires_in * 1000,
+        httpOnly: true,
+        secure: true,
+    });
     expect(response.status).toHaveBeenCalledWith(200);
-    expect(response.cookie).toHaveBeenCalledWith("api_key", request.body.apiKey);
+});
+
+it("should return error object if external request failed", async () => {
+    const response = getResponse();
+    const request = getRequest();
+    request.body.apiKey = "test_api_key";
+
+    const failedExternalResponse = { data: { status: 400, message: "invalid client" } };
+
+    axios.post.mockRejectedValueOnce({ response: failedExternalResponse });
+
+    await postAccess(context, request, response);
+
+    expect(response.status).toHaveBeenCalledWith(failedExternalResponse.data.status);
+    expect(response.json).toHaveBeenCalledWith({
+        error: "external_request",
+        message: failedExternalResponse.data.message,
+    });
 });
