7e265c709850c401df4dd88d1dda9d2fea174b2f
danieltriphaus <d.triphaus@outlook.de>
2022-03-07 15:12:50 +0100

    implement game search api endpoint

---
+++ src/api/handlers/getGameSearch.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const query =
+    'search "{query}";' +
+    "fields name, first_release_date, cover.height, cover.width, cover.url, involved_companies.developer, involved_companies.company.name;" +
+    "where involved_companies.developer = true;";
+
+export const getGameSearch = async (context, req, res) => {
+    const response = await axios.post("https://api.igdb.com/v4/games", query.replace("{query}", req.query.q), {
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "text/plain",
+            "Client-ID": process.env.IGDB_API_CLIENT_ID,
+            Authorization: "Bearer " + req.cookies.igdb_access_token,
+        },
+    });
+
+    res.status(200).json(response.data);
+};
--- src/api/middleware/apiBackend.js
+++ src/api/middleware/apiBackend.js
@@ -2,15 +2,17 @@ import { OpenAPIBackend } from "openapi-backend";
 
 import { postAccess } from "../handlers/postAccess.js";
 import { getGame } from "../handlers/getGame.js";
+import { getGameSearch } from "../handlers/getGameSearch.js";
 
 const api = new OpenAPIBackend({
     definition: "src/api/schema/GameReleaseCalendar.json",
     handlers: {
+        "get-game-search": getGameSearch,
+        "post-access": postAccess,
+        "get-game": getGame,
         "get-access": (context, req, res) => {
             res.status(200).end();
         },
-        "post-access": postAccess,
-        "get-game": getGame,
         notFound: (context, req, res) => {
             res.status(404).json("API Route not found");
         },
