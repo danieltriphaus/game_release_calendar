b9a74c07b73bd6680482fa35c4809d94197f530d
danieltriphaus <d.triphaus@outlook.de>
2022-12-11 14:03:46 +0100

    fix add temporary game test

--- tests/unit/frontend/components/AddTemporaryGame.spec.js
+++ tests/unit/frontend/components/AddTemporaryGame.spec.js
@@ -4,32 +4,20 @@ import AddTemporaryGame from "@/frontend/components/AddTemporaryGame.vue";
 jest.mock("axios");
 
 const CONTROLS = {
-    openButton: "button[data-test='open-temp-game-control']",
     nameInput: "input[data-test='temp-game-name']",
     cancelButton: "button[data-test='cancel-input']",
 };
 
-it("should just display button on default", () => {
+it("cancel button should clear input", async () => {
     const wrapper = mount(AddTemporaryGame);
 
-    expect(wrapper.find(CONTROLS.nameInput).exists()).toBe(false);
-    expect(wrapper.get(CONTROLS.openButton).text()).toContain("Game not found?");
-});
+    const nameInput = wrapper.get(CONTROLS.nameInput);
+    await nameInput.setValue("test name");
 
-it("should toggle temporary game inputs and clear them", async () => {
-    const wrapper = mount(AddTemporaryGame);
+    const cancelButton = wrapper.get(CONTROLS.cancelButton);
+    await cancelButton.trigger("click");
 
-    await wrapper.get(CONTROLS.openButton).trigger("click");
+    expect(nameInput.element.value).toBeFalsy();
 
-    expect(wrapper.find(CONTROLS.openButton).exists()).toBe(false);
-    expect(wrapper.find(CONTROLS.nameInput).exists()).toBe(true);
-
-    await wrapper.get(CONTROLS.cancelButton).trigger("click");
-
-    expect(wrapper.find(CONTROLS.openButton).exists()).toBe(true);
-    expect(wrapper.find(CONTROLS.nameInput).exists()).toBe(false);
-
-    await wrapper.get(CONTROLS.openButton).trigger("click");
-
-    expect(wrapper.find(CONTROLS.nameInput));
+    nameInput;
 });
