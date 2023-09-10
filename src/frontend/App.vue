3fff4920a821c51a6e37f7315f6e7be870ea8118
danieltriphaus <d.triphaus@outlook.de>
2023-09-10 10:12:14 +0200

    added links to privacy notice

--- src/frontend/App.vue
+++ src/frontend/App.vue
@@ -15,6 +15,20 @@
         </div>
         <router-view v-if="isAuthenticated" />
     </div>
+    <nav class="navbar navbar-expand-lg text-bg-light position-fixed bottom-0 w-100 footer">
+        <div class="container-fluid">
+            <div class="navbar-nav flex-row">
+                <a
+                    class="nav-link p-2"
+                    href="/landingpage/imprint/"
+                >Imprint</a>
+                <a
+                    class="nav-link p-2"
+                    href="/landingpage/privacy_notice/"
+                >Privacy</a>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
