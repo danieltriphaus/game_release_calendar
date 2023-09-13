4bb6b09d77ac47c330c15270d8e4e2cfd3960269
danieltriphaus <d.triphaus@outlook.de>
2023-09-13 14:44:59 +0200

    redirect base domain to www

--- src/api/app.js
+++ src/api/app.js
@@ -35,6 +35,15 @@ router.use(apiBackend());
 app.use("/api", router);
 
 if (process.env.NODE_ENV === "production") {
+    app.all(/.*/, function (req, res, next) {
+        var host = req.header("host");
+        if (host.match(/^www\..*/i)) {
+            next();
+        } else {
+            res.redirect(301, req.protocol + "://www." + host);
+        }
+    });
+
     app.get("/app/*.js", function (req, res, next) {
         req.url = req.url + ".gz";
         res.set("Content-Encoding", "gzip");
