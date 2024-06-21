069c0385b3d9b0f75dec752f40c94d32f6456b02
danieltriphaus <d.triphaus@outlook.de>
2024-06-21 15:09:41 +0200

    removed jest and tests

--- tests/unit/api/library/chunkArray.spec.js
+++
@@ -1,7 +0,0 @@
-import { chunkArray } from "@/api/library/chunkArray";
-
-it("should return a new array with the elements of the given size", () => {
-    expect(chunkArray([1, 2, 3, 4], 2)).toMatchObject([[1, 2], [3, 4]]);
-    expect(chunkArray([1, 2, 3], 2)).toMatchObject([[1, 2], [3]]);
-    expect(chunkArray([1, 2, 3], 0)).toMatchObject([1, 2, 3]);
-});
